2fefe6f7053ff49d6816333dbeb0f464d45b5366
joyrexus <joyrexus@gmail.com>
2014-12-02 10:00:23 -0600

    tweak

--- make-me-hapi/problems/routes/server.js
+++ make-me-hapi/problems/routes/server.js
@@ -6,7 +6,9 @@ var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
 server.route({
     path: '/{name}',
     method: 'GET',
-    handler: function (request, reply) { reply('Hello ' + request.params.name); }
+    handler: function (request, reply) {
+        reply('Hello ' + request.params.name);
+    }
 });
 
 server.start();
