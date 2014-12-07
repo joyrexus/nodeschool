eeefcc7fe149720c5b027a7de736daf438131e52
joyrexus <joyrexus@gmail.com>
2014-12-07 08:18:28 -0600

    add alternate solution

---
+++ learn-you-node/03-io-sync/solution-2.js
@@ -0,0 +1,9 @@
+var fs = require('fs'),
+    filename = process.argv[2];
+
+var countLines = function (err, data) {
+   if (err) throw err;
+   console.log(data.split('\n').length - 1);
+};
+
+fs.readFile(filename, 'utf8', countLines);
